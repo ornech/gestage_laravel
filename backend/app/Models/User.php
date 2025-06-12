676048f8fb9d6cc86951bad963e8418b1b4d87fe
Jean-François ORNECH <jean-francois.ornech@ac-poitiers.fr>
2025-06-12 08:16:41 +0200

    [frontend] auth mis en place

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Models\User; 
+
 
 Route::get('/ping', function () {
     try {
@@ -16,4 +19,24 @@ Route::get('/ping', function () {
             'message' => $e->getMessage()
         ], 500);
     }
+});
+
+
+Route::middleware(['ensureFrontend', 'auth:sanctum'])->get('/me', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json(['error' => 'Identifiants invalides'], 401);
+    }
+
+    $token = $user->createToken('authToken')->plainTextToken;
+
+    return response()->json([
+        'access_token' => $token,
+        'token_type' => 'Bearer',
+    ]);
 });
